a4fb7789251e0e9ec3676c040afcb720200cabbf
demoB33 <vasilenko-s@list.ru>
2023-04-19 11:16:31 +0700

    Initial commit

---
+++ src/main/java/com/departments/streamhw/StreamHwApplication.java
@@ -0,0 +1,13 @@
+package com.departments.streamhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamHwApplication.class, args);
+	}
+
+}
